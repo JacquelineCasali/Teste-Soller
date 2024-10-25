bf36bf252a2456c4691356584167f6da175660ef
Jacqueline <jcasalia@yahoo.com.br>
2024-10-25 07:31:02 -0300

    map e carrosel

--- frontEnd/src/page/Home.jsx
+++ frontEnd/src/page/Home.jsx
@@ -1,10 +1,39 @@
-import System from "../components/system/System";
+// import System from "../components/system/System";
 
+import Footer from "../components/Footer/Footer"
+import Card from '../components/Card/Card'
+import Container from '../components/Container/Container'
+import Hearder from "../components/Header/Hearder"
+import { useState } from "react";
+import service from "../services/service.json"
+import system from "../services/system.json"
+import Singup from "../components/Singup/Singup";
 
 export default function Home() {
-  return (
-    <>
-      <System/>
+  const [systems] = useState(system);
+   const [servicea] = useState(service);
+
+    return (
+  
+  <>
+      <Hearder/>
+      {/* <System/> */}
+      
+ <Singup
+test={servicea}
+
+/>
+
+<Singup
+test={systems}
+
+/>
+
+
+<Card/>
+
+<Container/>
+     <Footer/>
     </>
   )
 }
